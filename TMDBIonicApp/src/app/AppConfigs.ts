a4b125aae9b7eaba4134a0676753177e71fa9d86
deep <deep9751@gmail.com>
2019-12-07 16:06:49 +0200

    deleted tests, added configs

---
+++ TMDBIonicApp/src/app/AppConfigs.ts
@@ -0,0 +1,5 @@
+export class AppConfigs {
+    public static readonly ApiKey: string = '176e4083ff5a86620b723c2db527bada';
+    public static readonly ApiUrl: string = 'https://api.themoviedb.org/3';
+    public static readonly ImagesPath: string = 'https://image.tmdb.org/t/p/w500';
+}
